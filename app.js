8ce6606c1cad915d6e0d815dc1c7e742d70b1934
Arthur Milliken <arthur.milliken@autodesk.com>
2017-04-20 10:12:55 -0700

    adds

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const bodyParser = require('body-parser'),
-      cookieParser = require('cookie-parser'),
-      cookieSession = require('cookie-session'),
-      ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn,
-      express = require('express'),
-      flash = require('connect-flash'),
-      fs = require('fs'),
-      https = require('https'),
-      passport = require('passport'),
-      LocalStrategy = require('passport-local').Strategy;
+const _ = require('lodash'),
+  bodyParser = require('body-parser'),
+  cookieParser = require('cookie-parser'),
+  cookieSession = require('cookie-session'),
+  debug = require('debug')('gateway:server'),
+  ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn,
+  express = require('express'),
+  flash = require('connect-flash'),
+  fs = require('fs'),
+  https = require('https'),
+  oauth2orize = require('oauth2orize'),
+  passport = require('passport'),
+  BasicStrategy = require('passport-http').BasicStrategy,
+  BearerStrategy = require('passport-http-bearer').Strategy,
+  ClientPasswordStrategy = require('passport-oauth2-client-password').Strategy,
+  LocalStrategy = require('passport-local').Strategy;
 
 const app = express();
+app.use([
+  (req, res, next) => { console.log('*** FIRST!  ***'); next(); },
+  (req, res, next) => { console.log('*** SECOND! ***'); res.send('hello world'); },
+  (req, res, next) => { console.log('*** THIRD!  ***'); next(); },
+]);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -17,7 +28,7 @@ app.use(cookieParser());
 app.use(cookieSession({
   name: 'session',
   secret: 'my very secret password',
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours   
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
 // app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 app.use(flash());
@@ -26,19 +37,71 @@ app.use(passport.session());
 
 app.set('view engine', 'ejs');
 
-passport.use(new LocalStrategy((user, pass, done) => {
-  if (user === 'arthur' && pass === 'hamlet')
-    done(null, {username: 'arthur'});
-  else
-    done(null, false, { message: 'Invalid username or password.'});
-}));
+const USER = {
+  username: 'arthur',
+  email: 'arthur@example.com'
+};
 
-passport.serializeUser((user, cb) => {
-  cb(null, user);
+const CLIENT = {
+  clientId: 'CLIENT_ID',
+  name: 'Client (TM)'
+};
+
+const SCOPE = {
+  scope: '*'
+};
+
+const authenticate = (user, pass, done) => {
+  if (user === 'arthur' && pass === 'password')
+    done(null, USER);
+  else done(null, false);
+};
+
+const oauth = oauth2orize.createServer();
+oauth.serializeClient((client, done) => {
+  debug(`oauth.serializeClient:${client}`);
+  return done(null, client);
 });
 
-passport.deserializeUser((user, cb) => {
-  cb(null, user);
+oauth.deserializeClient((client, done) => {
+  debug(`oauth.deserializeClient:${client}`);
+  return done(null, client);
+});
+
+oauth.exchange(oauth2orize.exchange.clientCredentials(function(client, scope, done) {
+  debug(`oauth.exchange:clientCredentials:${JSON.stringify(client)}`);
+    return done(null, 'TOKEN', { expires_in: 1800, scope: '*' });
+}));
+
+passport.use(new LocalStrategy((user, pass, done) => {
+  debug(`LocalStrategy:${user}:${pass}`);
+  authenticate(user, pass, done);
+}));
+
+passport.use(new BasicStrategy((user, pass, done) => {
+  debug(`BasicStrategy:${user}:${pass}`);
+  authenticate(user, pass, done);
+}));
+
+passport.use(new ClientPasswordStrategy((clientId, clientSecret, done) => {
+  debug(`ClientPasswordStrategy:${clientId}:${clientSecret}`);
+  authenticate(clientId, clientSecret, done);
+}));
+
+passport.use(new BearerStrategy((token, done) => {
+  debug(`BearerStrategy:${token}`);
+  if (token === 'TOKEN') return done(null, USER, SCOPE);
+  else return done(null, false);
+}));
+
+passport.serializeUser((user, done) => {
+  debug(`serializeUser:${user}`);
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  debug(`deserializeUser:${user}`);
+  done(null, user);
 });
 
 app.get('/', (req, res) => {
@@ -53,7 +116,7 @@ app.post('/login', passport.authenticate('local', {
   successRedirect: '/home',
   successFlash: 'Welcome!',
   failureRedirect: '/login',
-  failureFlash: true
+  failureFlash: 'Invalid Credentials.'
 }));
 
 app.get('/logout', (req, res) => {
@@ -61,26 +124,51 @@ app.get('/logout', (req, res) => {
   res.redirect('/login');
 });
 
-const authenticate = function (req, res, next) {
-  if (req.isAuthenticated()) return next();
-  res.redirect('/login');
-};
-
 app.get('/home', ensureLoggedIn('/login'), (req, res) => {
   console.log('/home:flash:', req.flash());
   console.log('/home:session:', req.session);
   res.render('home', { user: req.user });
 });
 
-app.get('/api', (req, res) => {
-  res.json({
-    hello: 'from api!',
+app.get('/basic',
+  passport.authenticate('basic', { session: false }),
+  (req, res) => {
+    res.json({
+      path: '/basic',
+      user: req.user,
+      authInfo: req.authInfo
+    });
+  }
+);
+
+const qbody = (req, res, next) => {
+  _.each(Object.keys(req.query), (key) => {
+    req.body[key] = req.body[key] || req.query[key];
   });
-});
+  next();
+};
+
+app.post('/oauth/token', [
+  qbody,
+  passport.authenticate(['basic', 'oauth2-client-password'], { session: false }),
+  oauth.token(),
+  oauth.errorHandler()
+]);
+
+app.get('/api',
+  passport.authenticate('bearer', { session: false }),
+  (req, res) => {
+    res.json({
+      path: '/api',
+      user: req.user,
+      authInfo: req.authInfo
+    });
+  }
+);
 
 // const PORT = process.env.PORT || 3001;
 // app.listen(PORT, () => {
-//   console.log(`http: listening on port ${PORT}`);  
+//   debug(`http: listening on port ${PORT}`);  
 // });
 
 const PORT = process.env.PORT || 3001;
@@ -88,5 +176,5 @@ https.createServer({
   key: fs.readFileSync(__dirname + '/secrets/key.pem'),
   cert: fs.readFileSync(__dirname + '/secrets/cert.pem'),
 }, app).listen(PORT, () => {
-  console.log(`https: listening on port ${PORT}`); 
+  debug(`https: listening on port ${PORT}`); 
 });
